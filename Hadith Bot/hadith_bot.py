eb3293a5acce5faef82c410b2cc1d1299fdbf0f3
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-05 23:55:25 +0100

    update

--- Hadith Bot/hadith_bot.py
+++ Hadith Bot/hadith_bot.py
@@ -1,1 +1,12 @@
+import requests
+# get api request link from sunnah.com
+url = "https://api.sunnah.com/v1/hadiths/random"
 
+payload = "{}"
+headers = {'x-api-key': 'SqD712P3E82xnwOAEOkGd5JZH8s9wRR24TqNFzjk'}
+
+hadith_response = requests.request("GET", url, data=payload, headers=headers)
+
+json_hadith_response = hadith_response.json()
+
+print(json_hadith_response)
