5bde762e036a08cefa2ecda04cb5647bd3cbe31e
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-06 01:18:46 +0100

    Update hadith_bot.py

--- Hadith Bot/hadith_bot.py
+++ Hadith Bot/hadith_bot.py
@@ -2,59 +2,43 @@ import requests
 from telegram.ext import CommandHandler, Updater, MessageHandler, Filters, CallbackQueryHandler
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
-updater = Updater(token="1682743874:AAFe-9mXdjoM0l9_77SYAmn5AG0Kqo7pp4k")
+updater = Updater(token="1682743874:AAFe-9mXdjoM0l9_77SYAmn5AG0Kqo7pp4k", use_context= True)
 dispatcher = updater.dispatcher
 
-def choices(bot, update):
-    options = [
-            [InlineKeyboardButton("Get a random Hadith", callback_data="randomhadiths"),
+def start(update, context):
+    context.bot.send_message(chat_id = update.effective_chat.id, text="Get a random hadiths")
 
-            ]
-    ]
+dispatcher.add_handler(CommandHandler("start", start))
 
-    reply = InlineKeyboardMarkup(options)
-    bot.send_message(chat_id=update.message.chat_id, text = "Choose an option", reply_markup = reply)
+def get_hadith():
 
-dispatcher.add_handler(CommandHandler("hadiths", choices))
+    # get api request link from sunnah.com
+    url = "https://api.sunnah.com/v1/hadiths/random"
 
-def get_hadith(bot, update):
+    # declaring data dictionary and required header key
+    payload = "{}"
+    headers = {'x-api-key': 'SqD712P3E82xnwOAEOkGd5JZH8s9wRR24TqNFzjk'}
 
-    data = update.callback_query.data
-    hadith = ""
+    # getting a random hadiths from the api call
+    hadith_response = requests.request("GET", url, data=payload, headers=headers)
 
-    if (data == "randomhadiths"):
+    # converting the response data into json format acting as a dictionary
+    json_hadith_response = hadith_response.json()
 
-        # get api request link from sunnah.com
-        url = "https://api.sunnah.com/v1/hadiths/random"
+    # cleaning the data
+    hadith_collection = json_hadith_response['collection']
+    hadith_book_number = json_hadith_response['bookNumber']
+    hadith_topic = json_hadith_response['hadith'][0]['chapterTitle']
+    hadith_body = json_hadith_response['hadith'][0]['body']
+    hadith_body_edited = hadith_body.replace("<br/>","").strip("<p></p>").replace("<b>","").replace("</b>","")
+    
+    random_hadith = f"From Collection of {hadith_collection.title()}\n Book of {hadith_book_number} \
+        \nUnder the topic:{hadith_topic}\n HADITH:{hadith_body_edited}"
+    return random_hadith 
+def result(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=get_hadith())
 
-        # declaring data dictionary and required header key
-        payload = "{}"
-        headers = {'x-api-key': 'SqD712P3E82xnwOAEOkGd5JZH8s9wRR24TqNFzjk'}
-
-        # getting a random hadiths from the api call
-        hadith_response = requests.request("GET", url, data=payload, headers=headers)
-
-        # converting the response data into json format acting as a dictionary
-        json_hadith_response = hadith_response.json()
-
-        # cleaning the data
-        hadith_collection = json_hadith_response['collection']
-        hadith_book_number = json_hadith_response['bookNumber']
-        hadith_topic = json_hadith_response['hadith'][0]['chapterTitle']
-        hadith_body = json_hadith_response['hadith'][0]['body']
-        hadith_body_edited = hadith_body.replace("<br/>","").strip("<p></p>").replace("<b>","").replace("</b>","")
-
-    bot.edit_message_text(chat_id = update.callback_query.message.chat_id,
-            text = hadith,
-            message_id = update.callback_query.message.message_id)
-
-dispatcher.add_handler(CallbackQueryHandler(get_hadith))
-
+dispatcher.add_handler(CommandHandler("randomhadith", result))
 
 updater.start_polling()
 updater.idle()
-
-#print(f"From Collection of {hadith_collection.title()}")
-#print(f"Book of {hadith_book_number}")
-#print(f"Under the topic:{hadith_topic}")
-#print(f"Hadith:{hadith_body_edited}")
