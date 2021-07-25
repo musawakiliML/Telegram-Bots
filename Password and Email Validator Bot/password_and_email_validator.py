92804ea4e96ec4d9cff2ab4139c88d4869c585a3
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-07-26 00:49:59 +0100

    Update password_and_email_validator.py

--- Password and Email Validator Bot/password_and_email_validator.py
+++ Password and Email Validator Bot/password_and_email_validator.py
@@ -21,16 +21,45 @@ def email_validator(email):
     else:
         return f"{email} is not a valid email"
 
-#email_validator("musaww@hhhe")
+# creating the strong password validator function
+def strong_password_validator(password):
+    # creating the strong password regex
+    strong_password = re.compile(r'''(
+        ^(?=.*[A-Z].*[A-Z])     # at least two characters
+        (?=.*[!@#$&*])         # at least one of the special characters
+        (?=.*[0-9].*[0-9])     # at least two numeric digits
+        (?=.*[a-z].*[a-z].*[a-z]) # at least three lower case characters
+        .{8,}                     # at least a total of eight characters 
+        $
+    )''', re.VERBOSE)
+    while True:
+        mo = strong_password.search(password)
+        if (not mo):
+            return f"Your password should have at least one special charachter, two digits.\
+                    At least two uppercase and three lowercase charachter.\
+                    It Should be 8+ characters in lenght.\
+                    Enter another password:"
+        else:
+            print("Password is strong")
+            return
+
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("This bot validates email addresses. You Just write the email and it will tell you\
-        whether it is valid or not!")
+        whether it is valid or not! Select \password or \email")
+def email(update: Update, context: CallbackContext):
+    update.message.reply_text("Enter a password to test:")
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, email_validator))
 
 def validator(update: Update, context: CallbackContext):
     update.message.reply_text(email_validator(update.message.text))
 
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, validator))
+def pass_validator(update:Update, context: CallbackContext):
+    update.message.reply_text(email_validator(update.message.text))
+
+
 dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("password", password))
+dispatcher.add_handler(CommandHandler("email", email))
 
 updater.start_polling()
 updater.idle()
